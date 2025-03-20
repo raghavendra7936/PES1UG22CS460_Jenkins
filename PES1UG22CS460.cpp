ab2d28d5b4a712c3832e2011aedecb1d46fbcebd
raghavendra7936 <118116240+raghavendra7936@users.noreply.github.com>
2025-03-20 09:40:29 +0530

    PES1UG22CS460.cpp

--- PES1UG22CS460.cpp
+++ PES1UG22CS460.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, This is PES1UG22CS460 , connecting to Jenkins file" << std::endl;
+    std::cout << "Hello, This is PES1UG22CS460, connecting to Jenkins file" << std::endl;
     return 0;
 }
-prog.cpp
